57f14878dfe0f56bef7d4fc1841f19aa2729e23b
Lisandro B <lisandro.bertoli@gmail.com>
2020-01-10 13:48:23 +0100

    Changes to variable names to follow conventions

--- CalculatorHTML/index.js
+++ CalculatorHTML/index.js
@@ -1,76 +1,73 @@
 // ====== Selectors ======== //
-var BUTTONS = document.querySelectorAll('button')
-var EQUALS_BUTTON = document.getElementById('equals');
-var AC = document.getElementById('ac');
-var DISPLAY = document.getElementById('display');
-var DEL = document.getElementById('borrar');
+var buttons = document.querySelectorAll('button');
+var equalsButton = document.getElementById('equals');
+var acButton = document.getElementById('ac');
+var display = document.getElementById('display');
+var delButton = document.getElementById('borrar');
 
 //-----
-var OLD_RESULT_DISPLAYED = false;
-var operationValues = ['+', '-', '*', '/']
+var oldResultDisplayed = false;
+var operationValues = ['+', '-', '*', '/'];
 //-----
 
 //=========== Adding Events to selections ======== //
 
-BUTTONS.forEach(function(button){
-    button.addEventListener('click', addValue)
+buttons.forEach(function(button) {
+	button.addEventListener('click', addValue);
 });
 
-EQUALS_BUTTON.addEventListener('click', function(event){
-    if(DISPLAY.value !== ""){
-        DISPLAY.value = eval(DISPLAY.value);
-        OLD_RESULT_DISPLAYED = true;
-    }
+equalsButton.addEventListener('click', function(event) {
+	if (display.value !== '') {
+		display.value = eval(display.value);
+		oldResultDisplayed = true;
+	}
 });
 
-AC.addEventListener('click', ac)
+acButton.addEventListener('click', ac);
 
-DEL.addEventListener('click', del);
+delButton.addEventListener('click', del);
 
 // ====== Functions ========== //
 
-function addValue(event){
-    
-    var lastChar = DISPLAY.value.slice(-1);
-    var value = event.target.value;
+function addValue(event) {
+	var lastChar = display.value.slice(-1);
+	var value = event.target.value;
 
-    if(!OLD_RESULT_DISPLAYED){
-        freshStart(value, lastChar)
-    } else  {
-        keepGoing(value);
-    }
+	if (!oldResultDisplayed) {
+		freshStart(value, lastChar);
+	} else {
+		keepGoing(value);
+	}
 }
 
 function freshStart(value, lastChar) {
-    if(event.target.className !== "operation"){
-        console.log(event)
-        DISPLAY.value += value
-    }
-    if(operationValues.includes(value)){
-        if(!operationValues.includes(lastChar) && DISPLAY.value !== ''){
-            DISPLAY.value += value;
-        }
-    }
+	if (event.target.className !== 'operation') {
+		console.log(event);
+		display.value += value;
+	}
+	if (operationValues.includes(value)) {
+		if (!operationValues.includes(lastChar) && display.value !== '') {
+			display.value += value;
+		}
+	}
 }
 
 function keepGoing(value) {
-    if(operationValues.includes(value)){
-        DISPLAY.value += value;
-        OLD_RESULT_DISPLAYED = false;
-    } else {
-        DISPLAY.value = "";
-        DISPLAY.value += value;
-        OLD_RESULT_DISPLAYED = false;
-    } 
+	if (operationValues.includes(value)) {
+		display.value += value;
+		oldResultDisplayed = false;
+	} else {
+		display.value = '';
+		display.value += value;
+		oldResultDisplayed = false;
+	}
 }
 
 function ac() {
-    DISPLAY.value = "";
-};
-
-function del(){
-    var newString = DISPLAY.value.slice(0, -1);
-    DISPLAY.value = newString;
+	display.value = '';
 }
 
-
+function del() {
+	var newString = display.value.slice(0, -1);
+	display.value = newString;
+}
